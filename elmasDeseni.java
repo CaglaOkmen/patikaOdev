f40156d55c9a80c2dfcb4cb3a9d156b39447f07e
CaglaOkmen <126767927+CaglaOkmen@users.noreply.github.com>
2023-03-26 13:18:56 +0300

    Create elmasDeseni.java

---
+++ elmasDeseni.java
@@ -0,0 +1,30 @@
+// elmas deseni yazdirma
+import java.util.Scanner;
+
+public class elmasDeseni {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("sayi giriniz: ");
+        int n = scanner.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2 * i + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2 * i + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+    }
+}
